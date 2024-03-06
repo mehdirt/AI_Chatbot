a148253d919d63991283742669320414394848cd
Mahdi Rafati <mahdirafati680@gmail.com>
2024-03-06 17:28:52 +0330

    chat & qa functions added

--- AI_Chatbot/AI_Chatbot.py
+++ AI_Chatbot/AI_Chatbot.py
@@ -1,10 +1,8 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 from rxconfig import config
 
 import reflex as rx
 
-docs_url = "https://reflex.dev/docs/getting-started/introduction"
+
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
@@ -12,23 +10,34 @@ class State(rx.State):
     """The app state."""
 
 
-def index() -> rx.Component:
-    return rx.center(
-        rx.theme_panel(),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            align="center",
-            spacing="7",
-            font_size="2em",
-        ),
-        height="100vh",
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(question, text_align="right"),
+        rx.box(answer, text_align="left"),
+        margin_y='1em',
     )
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer) 
+            for question, answer in qa_pairs
+        ]
+    )
+
+
+def index() -> rx.Component:
+    return rx.container(chat())
 
 
 app = rx.App()
