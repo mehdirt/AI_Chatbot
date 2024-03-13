47066ddfd529f9589882163f0fac0879dc410405
Mahdi Rafati <mahdirafati680@gmail.com>
2024-03-13 17:17:28 +0330

    OpenAI API added

--- AI_Chatbot/state.py
+++ AI_Chatbot/state.py
@@ -1,29 +1,48 @@
-import asyncio
+import os
+from openai import OpenAI
 import reflex as rx
+
+OpenAI.api_key = os.environ["OPENAI_API_KEY"]
 
 class State(rx.State):
     """The app state."""
     # The current question being asked
     question: str
-
     # Keep track of the chat history as a list of (question, answer) tuples
     chat_history: list[tuple[str, str]]
 
-    async def answer(self):
-        #...
-        bot_answer = "I don't know"
+    def answer(self):
+        # 
+        client = OpenAI()
+        session = client.chat.completion.create(
+            model="gpt-3.5-turbo",
+            messages = [
+                {"role": "user", "content": self.question},
+            ],
+            stop=None,
+            temprature=0.7,
+            stream=True,
+        )
+
+        # Add to the answer as the chatbot responds
+        bot_answer = ""
         self.chat_history.append((self.question, bot_answer)) 
         # Clear the question input
         self.question = ""
         # Yield here to clear the frontend input before continuing
         yield
 
-        for i in range(len(bot_answer)):
+        for item in session:
+            if hasattr(item.choices[0].delta, "content"):
+                if item.choices[0].delta.content is None:
+                    # Presence of 'None' indicates the end of the response
+                    break
+
             # Pause to show the streaming effect
-            await asyncio.sleep(0.1)
+            bot_answer += item.choices[0].delta.content
             # Add one letter at a time to the output
             self.chat_history[-1] = (
                 self.chat_history[-1][0],
-                bot_answer[: i+1]
+                bot_answer
             )
             yield
